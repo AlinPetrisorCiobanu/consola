15d1ec698bab487964b4ea3c724560b565c1fd16
Rumanito12 <ady_9697@yahoo.com>
2023-10-05 22:45:38 +0200

    finalización responsive, empezando codigo Js

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,9 +1,18 @@
 
+    let onOff;
+    const start=()=>{
+        
+        if(onOff){
+            document.getElementById('gameVisualy').style.display='none'
+                onOff=false;
+        }else{
+            document.getElementById('gameVisualy').style.display='block'
+                onOff=true;
+        }
+    
+    }
     const L1=()=>{
         console.log("L1");
-    }
-    const start=()=>{
-        console.log("start");
     }
     const R1=()=>{
         console.log("R1");
